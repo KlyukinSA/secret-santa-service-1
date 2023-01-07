0e706034033458b14f815bb8f6a0301fe720a877
Ilya Tampio <quakumei@gmail.com>
2023-01-08 02:03:07 +0300

    add: client: loop and exception handling

--- client_project/src/main.rs
+++ client_project/src/main.rs
@@ -19,125 +19,146 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let address = address.lines().next().unwrap().to_string();
     println!("Server: {}", address);
 
-    // Выбрать метод
-    let mut method = String::new();
-    println!("Choose method:");
-    for i in 0..METHODS.len() {
-        println!("{} - {}", i, METHODS[i]);
-    }
-    let mut method_num;
+    let mut iteration = 0;
+
     loop {
-        std::io::stdin().read_line(&mut method)?;
-        method_num = method.trim().parse::<usize>()?;
-        if method_num < METHODS.len() {
+        println!("\nIteration: {}", iteration);
+        // Выбрать метод
+        let mut method = String::new();
+        println!("Choose method:");
+        for i in 0..METHODS.len() {
+            println!("{} - {}", i, METHODS[i]);
+        }
+        let mut method_num;
+        loop {
+            std::io::stdin().read_line(&mut method)?;
+            method_num = method.trim().parse::<usize>()?;
+            if method_num < METHODS.len() {
+                break;
+            }
+            println!("Wrong method number");
+        }
+
+        let mut command = String::new();
+        let mut command_num;
+        println!("Choose command:");
+        match METHODS[method_num] {
+            "GET" => {
+                for i in 0..GET_COMMANDS.len() {
+                    println!("{} - {}", i, GET_COMMANDS[i]);
+                }
+                loop {
+                    std::io::stdin().read_line(&mut command)?;
+                    command_num = command.trim().parse::<usize>()?;
+                    if command_num < GET_COMMANDS.len() {
+                        break;
+                    }
+                    println!("Wrong command number");
+                }
+            },
+            "POST" => {
+                for i in 0..POST_COMMANDS.len() {
+                    println!("{} - {}", i, POST_COMMANDS[i]);
+                }
+                loop {
+                    std::io::stdin().read_line(&mut command)?;
+                    command_num = command.trim().parse::<usize>()?;
+                    if command_num < POST_COMMANDS.len() {
+                        break;
+                    }
+                    println!("Wrong command number");
+                }
+            },
+            "PUT" => {
+                for i in 0..PUT_COMMANDS.len() {
+                    println!("{} - {}", i, PUT_COMMANDS[i]);
+                }
+                loop {
+                    std::io::stdin().read_line(&mut command)?;
+                    command_num = command.trim().parse::<usize>()?;
+                    if command_num < PUT_COMMANDS.len() {
+                        break;
+                    }
+                    println!("Wrong command number");
+                }
+            },
+            "DELETE" => {
+                for i in 0..DELETE_COMMANDS.len() {
+                    println!("{} - {}", i, DELETE_COMMANDS[i]);
+                }
+                loop {
+                    std::io::stdin().read_line(&mut command)?;
+                    command_num = command.trim().parse::<usize>()?;
+                    if command_num < DELETE_COMMANDS.len() {
+                        break;
+                    }
+                    println!("Wrong command number");
+                }
+            },
+            _ => {
+                println!("Wrong method");
+                return Ok(());
+            }
+        }
+
+        // Ввод json тела запроса
+        let mut json = String::new();
+        println!("Enter json body string, none is ok:");
+        std::io::stdin().read_line(&mut json)?;
+        let json = json.trim().to_string();
+
+        // Отправка запроса
+        let mut req = hyper::Request::new(hyper::Body::from(json));
+        *req.method_mut() = match METHODS[method_num] {
+            "GET" => hyper::Method::GET,
+            "POST" => hyper::Method::POST,
+            "PUT" => hyper::Method::PUT,
+            "DELETE" => hyper::Method::DELETE,
+            _ => {
+                println!("Wrong method");
+                return Ok(());
+            }
+        };
+        *req.uri_mut() = match METHODS[method_num] {
+            "GET" => format!("{}/{}", address, GET_COMMANDS[command_num]).parse()?,
+            "POST" => format!("{}/{}", address, POST_COMMANDS[command_num]).parse()?,
+            "PUT" => format!("{}/{}", address, PUT_COMMANDS[command_num]).parse()?,
+            "DELETE" => format!("{}/{}", address, DELETE_COMMANDS[command_num]).parse()?,
+            _ => {
+                println!("Wrong method");
+                return Ok(());
+            }
+        };
+
+        // Вывод запроса
+        println!("====================");
+        println!("Request: {}", req.method());
+        println!("Uri: {}", req.uri());
+
+        // Отправка запроса и обработать исключение если hyper::Error(IncompleteMessage)
+        let res = match client.request(req).await {
+            Ok(res) => res,
+            Err(e) => {
+                println!("Error: {}", e);
+                continue;
+            }
+        };
+
+
+        // Вывод ответа
+        println!("Response: {}", res.status());
+        let body = hyper::body::to_bytes(res).await?;
+        println!("Body: {}", std::str::from_utf8(&body)?);
+
+        // Повторить запрос
+        let mut repeat = String::new();
+        println!("Repeat? (y/n)");
+        std::io::stdin().read_line(&mut repeat)?;
+        if repeat.trim() == "n" {
             break;
         }
-        println!("Wrong method number");
+
+        iteration += 1;
     }
-
-    let mut command = String::new();
-    let mut command_num;
-    println!("Choose command:");
-    match METHODS[method_num] {
-        "GET" => {
-            for i in 0..GET_COMMANDS.len() {
-                println!("{} - {}", i, GET_COMMANDS[i]);
-            }
-            loop {
-                std::io::stdin().read_line(&mut command)?;
-                command_num = command.trim().parse::<usize>()?;
-                if command_num < GET_COMMANDS.len() {
-                    break;
-                }
-                println!("Wrong command number");
-            }
-        },
-        "POST" => {
-            for i in 0..POST_COMMANDS.len() {
-                println!("{} - {}", i, POST_COMMANDS[i]);
-            }
-            loop {
-                std::io::stdin().read_line(&mut command)?;
-                command_num = command.trim().parse::<usize>()?;
-                if command_num < POST_COMMANDS.len() {
-                    break;
-                }
-                println!("Wrong command number");
-            }
-        },
-        "PUT" => {
-            for i in 0..PUT_COMMANDS.len() {
-                println!("{} - {}", i, PUT_COMMANDS[i]);
-            }
-            loop {
-                std::io::stdin().read_line(&mut command)?;
-                command_num = command.trim().parse::<usize>()?;
-                if command_num < PUT_COMMANDS.len() {
-                    break;
-                }
-                println!("Wrong command number");
-            }
-        },
-        "DELETE" => {
-            for i in 0..DELETE_COMMANDS.len() {
-                println!("{} - {}", i, DELETE_COMMANDS[i]);
-            }
-            loop {
-                std::io::stdin().read_line(&mut command)?;
-                command_num = command.trim().parse::<usize>()?;
-                if command_num < DELETE_COMMANDS.len() {
-                    break;
-                }
-                println!("Wrong command number");
-            }
-        },
-        _ => {
-            println!("Wrong method");
-            return Ok(());
-        }
-    }
-
-    // Ввод json тела запроса
-    let mut json = String::new();
-    println!("Enter json body string, none is ok:");
-    std::io::stdin().read_line(&mut json)?;
-    let json = json.trim().to_string();
-
-    // Отправка запроса
-    let mut req = hyper::Request::new(hyper::Body::from(json));
-    *req.method_mut() = match METHODS[method_num] {
-        "GET" => hyper::Method::GET,
-        "POST" => hyper::Method::POST,
-        "PUT" => hyper::Method::PUT,
-        "DELETE" => hyper::Method::DELETE,
-        _ => {
-            println!("Wrong method");
-            return Ok(());
-        }
-    };
-    *req.uri_mut() = match METHODS[method_num] {
-        "GET" => format!("{}/{}", address, GET_COMMANDS[command_num]).parse()?,
-        "POST" => format!("{}/{}", address, POST_COMMANDS[command_num]).parse()?,
-        "PUT" => format!("{}/{}", address, PUT_COMMANDS[command_num]).parse()?,
-        "DELETE" => format!("{}/{}", address, DELETE_COMMANDS[command_num]).parse()?,
-        _ => {
-            println!("Wrong method");
-            return Ok(());
-        }
-    };
-
-    // Вывод запроса
-    println!("====================");
-    println!("Request: {}", req.method());
-    println!("Uri: {}", req.uri());
-
-    // Отправка запроса
-    let res = client.request(req).await?;
-
-    // Вывод ответа
-    println!("Response: {}", res.status());
-    let body = hyper::body::to_bytes(res).await?;
-    println!("Body: {}", std::str::from_utf8(&body)?);
-
     Ok(())
 }
